5428598e037bbd7a30924348545b360d794e68e6
gabrielmrt <gabrielmartimendez12@gmail.com>
2025-12-02 13:18:26 +0100

    Primer commit
    Primer commit con la clase ciclo.java

---
+++ src/ciclo.java
@@ -0,0 +1,8 @@
+
+public class ciclo {
+
+	public static void main(String[] args) {
+
+		System.out.println("DAW");
+	}
+}
---
+++ src/nombre.java
@@ -0,0 +1,8 @@
+
+public class nombre {
+	public static void main(String[] args) {
+
+		System.out.println("Gabriel Martin Mendez");
+	}
+
+}
